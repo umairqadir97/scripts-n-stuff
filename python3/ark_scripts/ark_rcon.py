9ced4126f8c86771bc24d945692d97200c9b2b60
lrountree <lredvtree@gmail.com>
2020-05-05 20:51:44 -0700

    Bug fixing and more comments etc for ark_rcon.py

--- python3/ark_scripts/ark_rcon.py
+++ python3/ark_scripts/ark_rcon.py
@@ -27,84 +27,117 @@ from struct import pack, unpack, calcsize
 
 # Populate variables
 Buffer = 4096
-packet = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#packet = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #Log_File = '/home/steam/3lemontrees/ShooterGame/Saved/Logs/rcon-' + datetime.datetime.now().strftime('%Y.%m.%d-%H.%M.%S' + '.log'
 
 # Set up log/exit function
 #log = log('rcon_logger', Log_File, 'critical', 'debug')
 
 # Set up socket operations as a class
-class tcp():
-    """help file here
+class tcp:
+    """Proper packet construction for Ark rcon servers.
+
+    -- Usage --
+    Command Line:
+        ark_rcon.py -H <host name or ip> -P <rcon port> -p <rcon password> -c <command request>
+    Module:
+    import ark_rcon
+    connect = ark_rcon.rcp(<host name or ip>, <rcon port>, <rcon password>)
+    rcon = connect.command
+    rcon(<command request>)
+
+    -- Defaults --
+    Host: localhost
+    Port: 32330
+    Pass: False (do not attempt auth)
+
+    -- Required Input --
+    Command Request
     """
+    # Initialize class and grab connection/authentication values
     def __init__(self, Host = 'localhost', Port = 32330, Pass = False):
+        self.packet = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.Host = Host
         self.Port = Port
         self.Pass = Pass
     
-    def get_response():
+    # Set up function to grab rcon server response
+    def get_response(self):
         Iteration = 0
         while Iteration < 11:
-            Respose = packet.recv(calcsize('<3i'))
+            Response = self.packet.recv(calcsize('<3i'))
             if len(Response) != 0:
                 break
             Iteration += 1
-            if Response:
-                (SIZE, ID, TYPE) = unpack('<3i', Response)
-                BODY = packet.recv (SIZE - 8)
-                return SIZE, ID, TYPE, BODY
-            else:
-                return False
+        if Response:
+            (SIZE, ID, TYPE) = unpack('<3i', Response)
+            BODY = self.packet.recv(SIZE - 8)
+            return (SIZE, ID, TYPE, BODY)
+        else:
+            return False
     
-    def create_message(ID,BODY,TYPE):
+    # Ark rcon requires a very specific packet format, set up function to create and pack the packet correctly
+    def create_message(self,ID,BODY,TYPE):
         Size = len(BODY) + 10 # get packet size
         Message = BODY.encode('utf-8') # encode message body
-        return pack('<3i{0}s'.format(len(BODY)), Size, ID, Message) + b'\x00\x00' # format the packet for the rcon server
+        return pack('<3i{0}s'.format(len(BODY)), Size, ID, TYPE, Message) + b'\x00\x00' # format the packet for the rcon server
 
+    # Set up function to connect to and attempt authentication with the rcon server
     def auth(self):
         try:
-            Connection_Response = packet.connect((self.Host,self.Port)) # Connect to rcon host
+            self.packet.connect((self.Host,self.Port)) # Connect to rcon host
         except:
-            packet.close()
-            return str(sys.exc_info()[1])
+            self.packet.close()
+            return '[Connection_Response] ' + str(sys.exc_info()[1])
         else:
-            if Connection_Response[3].decode('utf-8') != 'Keep Alive': # Check response to connection request
-                return Connection_Response[3].decode('utf-8')
-            if Pass:
+            Connection_Response = self.get_response()[3].decode('utf-8').rstrip('\\x00')
+            if Connection_Response and 'Keep Alive' not in Connection_Response: # Check response to connection request
+                self.packet.close()
+                return '[Connection_Response] ' + Connection_Response
+            elif not Connection_Response:
+                return '[Connection_Response] Received 10 empty responses from server.'
+            if self.Pass:
                 try:
-                    packet.send(self.tcp.create_message(3,self.Pass,3)) # Authenticate with rcon host
+                    self.packet.send(self.create_message(3,self.Pass,3)) # Authenticate with rcon host
                 except:
-                    packet.close()
-                    return str(sys.exc_info()[1])
+                    self.packet.close()
+                    return '[Authentication Send] ' + str(sys.exc_info()[1])
                 else:
-                    Auth_Response = self.tcp.get_response()
-                if Auth_Response[1] != 3: # Check authentication response
-                    packet.close()
-                    return Auth_Response[3].decode('utf-8')
+                    Auth_Response = self.get_response()
+                if Auth_Response and Auth_Response[1] != 3: # Check authentication response
+                    self.packet.close()
+                    return '[Auth_Response] ' + Auth_Response[3].decode('utf-8').rstrip('\\x00')
+                elif not Auth_Response:
+                    return '[Auth_Response] Received 10 empty responses from server.'
         return True
-
+    
+    # Set up function to send command request to rcon server
     def command(self, Command):
-        Auth = self.tcp.auth()
+        Auth = self.auth()
         if Auth != True:
-            packet_close()
+            self.packet.close()
             return Auth
         try:
-            packet.send(self.tcp.create_message(2,Command,2)) # Send command request to rcon host
+            self.packet.send(self.create_message(2,Command,2)) # Send command request to rcon host
         except:
-            packet_close()
-            return str(sys.exc_info()[1])
+            self.packet.close()
+            return '[Command Send] ' + str(sys.exc_info()[1])
         else:
-            Send_Response = self.tcp.get_response()
-        if Send_Response[1] != 2 or Send_Response[2] != 0:
-            packet_close()
+            Send_Response = self.get_response()
+        if Send_Response and Send_Response[1] != 2 or Send_Response[2] != 0: # Confirm we get the correct response, if not return an error
+            self.packet.close()
             return 'Unknown error- response ID or Type does not match command request! ID: {0}, Type: {1}'.format(Send_Response[1], Send_Response[2])
-        packet.close() # Close connection
-        return Send_Response[3].decode('utf-8') # Send server response, will contain response data or error data for processing later
+        elif not Send_Response:
+            self.packet.close()
+            return '[Send_Response] Received 10 empty responses from server.'
+        self.packet.close() # Close connection
+        return '[Send_Response] ' + Send_Response[3].decode('utf-8').rstrip('\\x00') # Send server response, will contain response data or error data for processing later
 
 # Run as script
 if __name__ == '__main__':
     import argparse
-
+    
+    # Set up argument parser to populate values
     parser = argparse.ArgumentParser(description='Connect, authenticate with, and send commands to the Ark rcon host server.', prog='ark_rcon')
     parser.add_argument('-H', action='store', default='localhost', help='Hostname or IP address of the rcon host. Default: localhost')
     parser.add_argument('-P', action='store', default='32330', help='Port used by the host for rcon communication. Default: 32330')
