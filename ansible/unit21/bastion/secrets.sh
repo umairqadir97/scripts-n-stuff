5bc007fbc8cf9f295b2f2a81be251306220dc24c
lrountree <lredvtree@gmail.com>
2021-10-05 23:59:10 -0700

    Adding ansible code for bastion stuff

---
+++ ansible/unit21/bastion/secrets.sh
@@ -0,0 +1,8 @@
+ENV={{ host_env }}
+INFLUX_TOKEN={{ influx_token }}
+INFLUX_BUCKET={{ influx_bucket }}
+PGSQL_URL={{ pgsql_url }}
+MSK_URL={{ msk_url }}
+CELERY_REDIS_HOST={{ celery_redis_host }}
+PATH_TO_REDIS_CLI={{ path_to_redis_cli }}
+ES_URL={{ es_url }}
